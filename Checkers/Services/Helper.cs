4d20865d5b0d178f4c7c7dd538cfa9df015398dd
RobyDoge <robyionut23@gmail.com>
2024-04-09 12:01:33 +0200

    Added XMLs, MultipleJumpsHandler Implemented

--- Checkers/Services/Helper.cs
+++ Checkers/Services/Helper.cs
@@ -13,7 +13,7 @@ namespace Checkers.Services
             var board = new ObservableCollection<ObservableCollection<Cell>>();
             var count = 0;
 
-            /*//laptop
+            //laptop
             for (var i = 0; i < 8; i++)
             {
                 var row = new ObservableCollection<Cell>();
@@ -24,49 +24,6 @@ namespace Checkers.Services
                                 @"D:\AN2-SEM2\MAP\MAP-Tema2\Checkers\bin\Debug\net8.0-windows\Images\BlackSpace_WhitePiece.png",
                                 @"D:\AN2-SEM2\MAP\MAP-Tema2\Checkers\bin\Debug\net8.0-windows\Images\BlackSpace_BlackPiece.png")
                             : new Cell(j, i, @"D:\AN2-SEM2\MAP\MAP-Tema2\Checkers\bin\Debug\net8.0-windows\Images\WhiteSpace_Empty.png"));
-                    row[j].CurrentImage = row[j].BackgroundEmptyPath;
-                    count++;
-                }
-                for (var j = 0; j < 8; j++)
-               {
-                   if (i < 3)
-                   {
-                       if (row[j].BackgroundEmptyPath.Contains("BlackSpace_Empty"))
-                       {
-                           row[j].CurrentImage = row[j].BlackPiece;
-                           row[j].CurrentState = State.BlackPiece;
-                           continue;
-                       }
-                       row[j].CurrentImage = row[j].BackgroundEmptyPath;
-                   }
-                   else if (i > 4)
-                   {
-                       if (row[j].BackgroundEmptyPath.Contains("BlackSpace_Empty"))
-                       {
-                           row[j].CurrentImage = row[j].WhitePiece;
-                           row[j].CurrentState = State.WhitePiece;
-                           continue;
-                       }
-                       row[j].CurrentImage = row[j].BackgroundEmptyPath;
-                   }
-                   row[j].CurrentImage = row[j].BackgroundEmptyPath;
-               }
-               board.Add(row);
-               count--;
-            }
-            */
-
-            //PC
-            for (var i = 0; i < 8; i++)
-            {
-                var row = new ObservableCollection<Cell>();
-                for (var j = 0; j < 8; j++)
-                {
-                    row.Add(count % 2 == 0
-                            ? new Cell(j,i,@"D:\1FACULTATE\Anu II\MAP\MAP-Checkers\Checkers\bin\Debug\net8.0-windows\Images\BlackSpace_Empty.png",
-                                @"D:\1FACULTATE\Anu II\MAP\MAP-Checkers\Checkers\bin\Debug\net8.0-windows\Images\BlackSpace_WhitePiece.png",
-                                @"D:\1FACULTATE\Anu II\MAP\MAP-Checkers\Checkers\bin\Debug\net8.0-windows\Images\BlackSpace_BlackPiece.png")
-                            : new Cell(j,i,@"D:\1FACULTATE\Anu II\MAP\MAP-Checkers\Checkers\bin\Debug\net8.0-windows\Images\WhiteSpace_Empty.png"));
                     row[j].CurrentImage = row[j].BackgroundEmptyPath;
                     count++;
                 }
@@ -97,6 +54,49 @@ namespace Checkers.Services
                 board.Add(row);
                 count--;
             }
+
+
+            //PC
+            //for (var i = 0; i < 8; i++)
+            //{
+            //    var row = new ObservableCollection<Cell>();
+            //    for (var j = 0; j < 8; j++)
+            //    {
+            //        row.Add(count % 2 == 0
+            //                ? new Cell(j,i,@"D:\1FACULTATE\Anu II\MAP\MAP-Checkers\Checkers\bin\Debug\net8.0-windows\Images\BlackSpace_Empty.png",
+            //                    @"D:\1FACULTATE\Anu II\MAP\MAP-Checkers\Checkers\bin\Debug\net8.0-windows\Images\BlackSpace_WhitePiece.png",
+            //                    @"D:\1FACULTATE\Anu II\MAP\MAP-Checkers\Checkers\bin\Debug\net8.0-windows\Images\BlackSpace_BlackPiece.png")
+            //                : new Cell(j,i,@"D:\1FACULTATE\Anu II\MAP\MAP-Checkers\Checkers\bin\Debug\net8.0-windows\Images\WhiteSpace_Empty.png"));
+            //        row[j].CurrentImage = row[j].BackgroundEmptyPath;
+            //        count++;
+            //    }
+            //    for (var j = 0; j < 8; j++)
+            //    {
+            //        if (i < 3)
+            //        {
+            //            if (row[j].BackgroundEmptyPath.Contains("BlackSpace_Empty"))
+            //            {
+            //                row[j].CurrentImage = row[j].BlackPiece;
+            //                row[j].CurrentState = State.BlackPiece;
+            //                continue;
+            //            }
+            //            row[j].CurrentImage = row[j].BackgroundEmptyPath;
+            //        }
+            //        else if (i > 4)
+            //        {
+            //            if (row[j].BackgroundEmptyPath.Contains("BlackSpace_Empty"))
+            //            {
+            //                row[j].CurrentImage = row[j].WhitePiece;
+            //                row[j].CurrentState = State.WhitePiece;
+            //                continue;
+            //            }
+            //            row[j].CurrentImage = row[j].BackgroundEmptyPath;
+            //        }
+            //        row[j].CurrentImage = row[j].BackgroundEmptyPath;
+            //    }
+            //    board.Add(row);
+            //    count--;
+            //}
             return board;
         }
 
---
+++ Checkers/XMLHandlers/MultipleJumpsHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml;
+
+namespace Checkers.XMLHandlers
+{
+    internal static class MultipleJumpsHandler
+    {
+        public static bool GetMultipleJumps()
+        {
+            XmlDocument xmlDoc = new();
+            xmlDoc.Load("../../../MultipleJumps.xml");
+            XmlNode? rootNode = xmlDoc.SelectSingleNode("MultipleJumpsAllowed");
+            if(rootNode == null) return false;
+            return rootNode.InnerText == "true";
+        }
+
+        public static bool ChangeMultipleJumps(bool multipleJumps)
+        {
+            var change = multipleJumps ? "true" : "false";
+            XmlDocument xmlDoc = new();
+            xmlDoc.Load("../../../MultipleJumps.xml");
+            XmlNode? rootNode = xmlDoc.SelectSingleNode("MultipleJumpsAllowed");
+            if(rootNode == null) return false;
+            rootNode.InnerText = change;
+            xmlDoc.Save("../../../MultipleJumps.xml");
+            return true;
+        }
+    }
+
+}
